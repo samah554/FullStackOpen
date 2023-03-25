a4c461c267f820d63aa2f1449eef76d5b86b51b1
Samah Shakir Ali <samah.ilfath@gmail.com>
2023-03-25 19:53:40 +0530

    part3.12

---
+++ part3/phonebook/person.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://samahFullStackOpen:${password}@cluster0.nn0gall.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: name,
+  number: number,
+});
+
+if (process.argv[3])
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+
+if (!process.argv[3]) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person);
+      mongoose.connection.close();
+    });
+  });
+}
